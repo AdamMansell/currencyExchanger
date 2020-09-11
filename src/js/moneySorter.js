4d862bc09dd5d867352733140b7ff0911e5e1693
AdamManselll <adammansellhmb@gmail.com>
2020-09-11 13:49:03 -0700

    Update moneySorter.js to now properly calculate currency exchange.

--- src/js/moneySorter.js
+++ src/js/moneySorter.js
@@ -1,14 +1,14 @@
 export function moneySorter(usd, altCur, moneyArray) {
   let selectedCurrency;
-  // console.log(usd, altCur, moneyArray);
+  
   altCur == "AUD" ? selectedCurrency = moneyArray.AUD : selectedCurrency;
   altCur == "EUR" ? selectedCurrency = moneyArray.EUR : selectedCurrency;
   altCur == "AED" ? selectedCurrency = moneyArray.AED : selectedCurrency;
   altCur == "CAD" ? selectedCurrency = moneyArray.CAD : selectedCurrency;
   altCur == "HKD" ? selectedCurrency = moneyArray.HKD : selectedCurrency;
   altCur == "JPY" ? selectedCurrency = moneyArray.JPY : selectedCurrency;
-
+  console.log(usd, altCur, moneyArray);
   let newAmount = parseFloat(usd * selectedCurrency);
-  // console.log(newAmount);
+  console.log(newAmount);
   return newAmount;
 }
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-// import { moneySorter } from "./js/moneySorter";
+import { moneySorter } from "./js/moneySorter";
 import "./css/styles.css";
 import $ from "jquery";
 import "bootstrap";
@@ -9,40 +9,31 @@ $(document).ready(function () {
     event.preventDefault();
     let altCur = $("#altCur").val();
     let usd = $("#usdVal").val();
-    console.log(usd);
-    console.log(altCur);
+    let promiseToRetrieveCurrencies = new Promise(function (resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
+      request.onload = function () {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+    promiseToRetrieveCurrencies.then(function (response) {
+      const allMoney = JSON.parse(response);
+      let moneyArray = allMoney.conversion_rates;
+      // console.log(moneyArray);
+      let result = moneySorter(usd, altCur, moneyArray);
+      $(".showDist").text(result);
+    },
+      function (error) {
+        const allMoney = JSON.parse(error);
+        // const astResponse = JSON.parse(error);
+        // console.log("hello");
+        $(".showDist").text(`error: ${allMoney.error_message}`);
+      });
   });
 });
-  
-// console.log(usd);
-
-// let altCur = $("#altCur").val();
-
-// let promiseToRetrieveCurrencies = new Promise(function (resolve, reject) {
-//   let request = new XMLHttpRequest();
-//   const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
-//   request.onload = function () {
-//     if (this.status === 200) {
-//       resolve(request.response);
-//     } else {
-//       reject(request.response);
-//     }
-//   };
-//   request.open("GET", url, true);
-//   request.send();
-// });
-// promiseToRetrieveCurrencies.then(function (response) {
-//   const allMoney = JSON.parse(response);
-//   let moneyArray = allMoney.conversion_rates;
-//   // console.log(moneyArray);
-//   moneySorter(usd, altCur, moneyArray);
-//   $(".showDist").text(`${moneySorter(moneyArray)}`);
-// },
-// function (error) {
-//   const allMoney = JSON.parse(error);
-//   // const astResponse = JSON.parse(error);
-//   // console.log("hello");
-//   $(".showDist").text(`error: ${allMoney.error_message}`);
-// });
-//   });
-// });
