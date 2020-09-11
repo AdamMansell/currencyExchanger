b4493addec96d8b07458d0de8de00ddd2e20605b
AdamManselll <adammansellhmb@gmail.com>
2020-09-11 09:28:30 -0700

    Add moneySorter.js for business logic, build out main.js to setup for API call.

---
+++ src/js/moneySorter.js
@@ -0,0 +1,3 @@
+export function moneySorter(moneyArray) {
+  return moneyArray;
+}
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import { moneySorter } from "./js/moneySorter";
 import $ from "jquery";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -5,6 +6,32 @@ import "./css/styles.css";
 
 $(document).ready(function () {
   $("#").click(function () {
+
+    let promise = new Promise(function (resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
+      request.onload = function () {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+
+    promise.then(function (response) {
+      const allMoney = JSON.parse(response);
+      let moneyArray = allMoney.conversion_rates;
+      moneySorter(moneyArray);
+      console.log(`${moneySorter(moneyArray)}`);
+      $(".showDist").text(`${moneySorter(moneyArray)}`);
+    }, function(error) {
+      const astResponse = JSON.parse(error);
+      console.log(astResponse);
+      $(".showDist").text(`error: ${allMoney.error_message}`);
+    });
 
   });
 });
