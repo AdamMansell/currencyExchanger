a956aa5665e7c523164f8e8f6174724ab4c8fce3
AdamManselll <adammansellhmb@gmail.com>
2020-09-11 10:39:59 -0700

    Properly retreieve list of currency objects with API call.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
-// import { moneySorter } from "./js/moneySorter";
+import { moneySorter } from "./js/moneySorter";
 import $ from "jquery";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
 
 $(document).ready(function () {
-  $("#").click(function () {
-
+  $("#getMoney").click(function () {
     let promise = new Promise(function (resolve, reject) {
       let request = new XMLHttpRequest();
       const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
@@ -20,20 +19,18 @@ $(document).ready(function () {
       request.open("GET", url, true);
       request.send();
     });
-
     promise.then(function (response) {
       const allMoney = JSON.parse(response);
       let moneyArray = allMoney.conversion_rates;
       console.log(moneyArray);
-      // moneySorter(moneyArray);
-  
-      // $(".showDist").text(`${moneySorter(moneyArray)}`);
+      moneySorter(moneyArray);
+      $(".showDist").text(`${moneySorter(moneyArray)}`);
+    },
+    function (error) {
+      const allMoney = JSON.parse(error);
+      const astResponse = JSON.parse(error);
+      console.log(astResponse);
+      $(".showDist").text(`error: ${allMoney.error_message}`);
     });
-    // function(error) {
-    //   const astResponse = JSON.parse(error);
-    //   console.log(astResponse);
-    //   $(".showDist").text(`error: ${allMoney.error_message}`);
-    // });
-
   });
 });
